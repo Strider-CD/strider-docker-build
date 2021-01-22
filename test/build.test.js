19486b61b08d66e8dd262bfbedf64d585d4e953b
caizx <caizx@chanjet.com>
2021-01-22 13:54:05 +0800

    fix: sort error

--- test/build.test.js
+++ test/build.test.js
@@ -9,7 +9,7 @@ describe("lib", () => {
   before(() => {
     mockServer = http.createServer((request, response) => {
       response.write(
-        JSON.stringify({ name: "xxxx", tags: ["latest"] })
+        JSON.stringify({ name: "xxxx", tags: ["latest", "1.7","1.3","1.5","1.10","1.4","1.2","1.9","1.8","1.1","1.6"] })
       );
       response.end();
     });
